8ea3d2baf0aafe57c6364f05845e194a77cb1933
Omar Hosny <honsyomar022@gmail.com>
2022-10-01 17:01:03 +0200

    nodemon added

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    res.end("home page");
+  } else if (req.url == "/about") {
+    res.end("about page");
+  } else {
+    res.end("error page");
+  }
+});
+
+server.listen(5000, () => {
+  console.log("Server is listening");
+});
